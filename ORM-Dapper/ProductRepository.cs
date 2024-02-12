3b1ef8006139c73f995a93044ef1ad5eca8f37f5
Myles Hill <mylehill99@gmail.com>
2024-02-12 16:54:56 -0500

    finished

---
+++ ORM-Dapper/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID)", new { name, price, categoryID});
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
